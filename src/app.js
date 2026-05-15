df700bf41e9de4ea1a54e68cf21360804b1267f9
Grazziel <Grazy_pamela@hotmail.com>
2026-05-15 10:06:24 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// aqui nos encontramos con los 4 ARRAYS ANIDADOS  y dentro de el STRINGS
+
+
+// Lista de pronombres que usaremos para formar los dominios
+let pronoun = ['the', 'our'];
+
+// Lista de adjetivos que se combinarán con los pronombres
+let adj = ['great', 'big'];
+
+// Lista de sustantivos que completarán el nombre del dominio
+let noun = ['jogger', 'racoon'];
+
+// Primer bucle: recorre cada pronombre del array 'pronoun'
+for (let p of pronoun) {
+
+  // Segundo bucle: recorre cada adjetivo del array 'adj'
+  for (let a of adj) {
+
+    // Tercer bucle: recorre cada sustantivo del array 'noun'
+    for (let n of noun) {
+
+      // Aquí concatenamos pronombre + adjetivo + sustantivo + la extensión .com
+      // y lo mostramos en consola
+      console.log(p + a + n + ".com");
+    }
+  }
+}
+
+// el resultado final se puede ver en la consola del navegador o en la terminal
