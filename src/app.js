868071d0795b488928b4ca24fd19f71a651342bc
Grazziel <Grazy_pamela@hotmail.com>
2026-05-15 10:53:37 +0000

    buenas practicas

--- src/app.js
+++ src/app.js
@@ -9,28 +9,33 @@ import "./assets/img/4geeks.ico";
 
 
 // Lista de pronombres que usaremos para formar los dominios
-let pronoun = ['the', 'our'];
+let pronouns = ['the', 'our'];
 
 // Lista de adjetivos que se combinarán con los pronombres
-let adj = ['great', 'big'];
+let adjs = ['great', 'big'];
 
 // Lista de sustantivos que completarán el nombre del dominio
-let noun = ['jogger', 'racoon'];
+let nouns = ['jogger', 'racoon'];
+
+// Lista de footers que completarán el nombre del dominio
+let footers = ['.com', '.es', '.net']
 
 // Primer bucle: recorre cada pronombre del array 'pronoun'
-for (let p of pronoun) {
+for (let pronoun of pronouns) {
 
   // Segundo bucle: recorre cada adjetivo del array 'adj'
-  for (let a of adj) {
+  for (let adj of adjs) {
 
     // Tercer bucle: recorre cada sustantivo del array 'noun'
-    for (let n of noun) {
+    for (let noun of nouns) {
 
-      // Aquí concatenamos pronombre + adjetivo + sustantivo + la extensión .com
-      // y lo mostramos en consola
-      console.log(p + a + n + ".com");
+      for (let footer of footers) {
+
+        // Aquí concatenamos pronombre + adjetivo + sustantivo + la extensión .com
+        // y lo mostramos en consola
+        console.log(pronoun + adj + noun + footer);
+      }
     }
   }
 }
-
 // el resultado final se puede ver en la consola del navegador o en la terminal
